24b7fb1ba9cb53636fa104a5f937a97623a9da50
congLyHome <ngcongly2002@gmail.com>
2025-03-02 01:39:35 +0700

    Solved problem 1 and 2 on LeetCode

---
+++ CPP/Scripts/LeetCode.cpp
@@ -0,0 +1,79 @@
+﻿// ReSharper disable all
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode* next;
+
+    ListNode() : val(0), next(nullptr)
+    {
+    }
+
+    ListNode(int x) : val(x), next(nullptr)
+    {
+    }
+
+    ListNode(int x, ListNode* next) : val(x), next(next)
+    {
+    }
+};
+
+class Solution
+{
+public:
+    // Problem 1: https://leetcode.com/problems/two-sum/
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        unordered_map<int, int> pairIdx;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int num = nums[i];
+            if (pairIdx.find(target - num) != pairIdx.end())
+            {
+                return {i, pairIdx[target - num]};
+            }
+            pairIdx[num] = i;
+        }
+
+        return {};
+    }
+
+    // Problem 2: https://leetcode.com/problems/add-two-numbers/
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        ListNode* head = l1;
+        ListNode* tail = nullptr;
+        int carry = 0;
+
+        while (l1 || l2 || carry) {
+            int sum = carry;
+            if (l1) {
+                sum += l1->val;
+                tail = l1;
+                l1 = l1->next;
+            }
+            if (l2) {
+                sum += l2->val;
+                l2 = l2->next;
+            }
+
+            tail->val = sum % 10;
+            carry = sum / 10;
+
+            if (!l1 && l2) {
+                tail->next = l2;
+                l1 = l2;
+                l2 = nullptr;
+            }
+
+            if (!l1 && carry) {
+                tail->next = new ListNode(carry);
+                break;
+            }
+        }
+
+        return head;
+    }
+};
